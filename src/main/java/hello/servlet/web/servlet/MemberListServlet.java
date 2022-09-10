df8d8b52344c2f80416028828b1e3559e9b09afa
oneal.oh <oneal.oh@kakaomobility.com>
2022-09-10 16:26:55 +0900

    update: new-form.jsp/save.jsp

--- src/main/java/hello/servlet/web/servlet/MemberListServlet.java
+++ src/main/java/hello/servlet/web/servlet/MemberListServlet.java
@@ -20,6 +20,9 @@ public class MemberListServlet extends HttpServlet {
     @Override
     protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
+        response.setContentType("text/html");
+        response.setCharacterEncoding("utf-8");
+
         List<Member> members = memberRepository.findAll();
         PrintWriter w = response.getWriter();
         w.write("<html>");
